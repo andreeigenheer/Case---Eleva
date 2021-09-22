3e129f81b727ddbf4fb4ee4c6ce34fa60ee34c49
Andre Eigenheer <eigenheer81@gmail.com>
2021-09-22 17:39:36 -0300

    Adição de rotas MENU

--- case/src/app/menu/menu.component.ts
+++ case/src/app/menu/menu.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlterarComponent } from '../alterar/alterar.component';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  alterar(){
-    console.log("Alterou")
+  alterar() {
+    console.log('Alterou');
+    this.router.navigateByUrl('alterar');
   }
 
-  ngOnInit(): void {
+  remover() {
+    console.log('Removeu');
+    this.router.navigateByUrl('remover');
   }
 
+  adicionar() {
+    console.log('adicionou');
+    this.router.navigateByUrl('novo-cadastro');
+  }
+
+  ngOnInit(): void {}
 }
